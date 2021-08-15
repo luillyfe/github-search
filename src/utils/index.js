d9944ddcc72c7bf3bc1a6348c6ed8e858483dc1d
Fermin Blanco <luillyfe89@gmail.com>
2021-08-14 21:34:28 -0500

    feat: showing notable information such bio, follower, following, login, name, location, profile picture.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,42 @@
-const url = "https://api.github.com/search/users";
+const searchURL = "https://api.github.com/search/users";
+const userURL = "https://api.github.com/user";
 
 export async function fetchUserPage(query, page = 1) {
   const limit = 10;
   const response = await fetch(
-    `${url}?q=${query}&per_page=${limit}&page=${page}`
+    `${searchURL}?q=${query}&per_page=${limit}&page=${page}`
   ).then((res) => res.json());
 
-  return { users: response.items, totalCount: response.total_count };
+  const usersResponse = await Promise.all(
+    response.items.map(({ id }) => {
+      return fetch(`${userURL}/${id}`).then((res) => res.json());
+    })
+  );
+
+  return {
+    users: usersResponse.map(
+      ({
+        name,
+        login,
+        id,
+        avatar_url: picture,
+        bio,
+        followers,
+        following,
+        location,
+        html_url: profileLink,
+      }) => ({
+        name,
+        login,
+        id,
+        picture,
+        bio,
+        followers,
+        following,
+        location,
+        profileLink,
+      })
+    ),
+    totalCount: response.total_count,
+  };
 }
